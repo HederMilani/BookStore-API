0be68460baaf00f3879aff4304fce779c345d2f0
Heder Milani <80220353+HederMilani@users.noreply.github.com>
2022-12-11 20:47:38 -0300

    ADD - Criar classes categorias e livros

---
+++ src/main/java/com/book/bookstore/domain/Categoria.java
@@ -0,0 +1,9 @@
+package com.book.bookstore.domain;
+
+public class Categoria {
+	
+	private Integer id;
+	private String name;
+	private String Description;
+
+}
---
+++ src/main/java/com/book/bookstore/domain/Livro.java
@@ -0,0 +1,11 @@
+package com.book.bookstore.domain;
+
+public class Livro {
+	
+	private Integer id;
+	private String title;
+	private String name_author;
+	private String text;
+
+
+}
