8cbc259e0eea6bcc0ac45c3fa9a059212d9cbb83
Heder Milani <80220353+HederMilani@users.noreply.github.com>
2022-12-11 21:26:47 -0300

    ADD - Metodos hash code categorias

--- src/main/java/com/book/bookstore/domain/Categoria.java
+++ src/main/java/com/book/bookstore/domain/Categoria.java
@@ -53,5 +53,31 @@ public class Categoria {
 	public void setLivro(List<Livro> livros){
 		this.livros = livros;
 	}
+
+	//Hash de verificação
+	@Override
+	public int hashCode(){
+		final int prime = 31;
+		int result = 1;
+		result = prime * ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj){
+		if(this == obj)
+			return true;
+		if(obj == null)
+			return false;
+		if(getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if(id == null){
+			if(other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	
 }
