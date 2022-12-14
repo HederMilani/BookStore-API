99dc40cd86c0f60aa9c7ae9b778fd113babc54bf
Heder Milani <80220353+HederMilani@users.noreply.github.com>
2022-12-14 16:56:37 -0300

    ADD - Metodo post para criar nova categoria

--- src/main/java/com/book/bookstore/resource/CategoriaResource.java
+++ src/main/java/com/book/bookstore/resource/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.book.bookstore.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,10 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.book.bookstore.domain.Categoria;
 import com.book.bookstore.dtos.CategoriaDTO;
 import com.book.bookstore.services.CategoriaService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping(value = "/categoria")
@@ -33,4 +38,12 @@ public class CategoriaResource {
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
 }
