8a53376041fda61af544dc9130c66338d50bfd8b
Heder Milani <80220353+HederMilani@users.noreply.github.com>
2022-12-13 23:27:43 -0300

    ADD - Criação método api get categoria(byId)

---
+++ src/main/java/com/book/bookstore/resource/CategoriaResource.java
@@ -0,0 +1,25 @@
+package com.book.bookstore.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.book.bookstore.domain.Categoria;
+import com.book.bookstore.services.CategoriaService;
+
+@RestController
+@RequestMapping(value = "/categoria")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Categoria> findById(@PathVariable Integer id){
+		Categoria obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
